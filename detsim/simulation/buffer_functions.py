47c129b7f23e8de8128480bd2f664512b82a4f27
Andrew Laing <a.b.r.laing@gmail.com>
2020-05-26 13:47:50 +0200

    Protect position_signal against events with no sipm info

--- detsim/simulation/buffer_functions.py
+++ detsim/simulation/buffer_functions.py
@@ -96,10 +96,12 @@ def calculate_buffers(buffer_len: float, pre_trigger: float,
                         sipm_bins  : np.ndarray,
                         sipm_charge:  pd.Series) -> List:
 
-        slice_and_pad = slice_generator(pmt_bins                      ,
-                                        np.array(pmt_charge .tolist()),
-                                        sipm_bins                     ,
-                                        np.array(sipm_charge.tolist()))
+        sipm_q = np.zeros_like(sipm_bins)\
+          if sipm_charge.empty else np.array(sipm_charge.tolist())
+        slice_and_pad = slice_generator(pmt_bins                     ,
+                                        np.array(pmt_charge.tolist()),
+                                        sipm_bins                    ,
+                                        sipm_q                       )
 
         return [(padder(*pmts), padder(*sipms))
                 for pmts, sipms in slice_and_pad(triggers)]
